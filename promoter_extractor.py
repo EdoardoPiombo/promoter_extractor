22dca40f06572e651a2dc2824dabbbf1c0ffbd3b
EdoardoPiombo <edoardo.piombo@gmail.com>
2021-12-01 17:05:25 +0100

    Update promoter_extractor.py

--- promoter_extractor.py
+++ promoter_extractor.py
@@ -7,7 +7,7 @@ parser = ap.ArgumentParser()
 parser.add_argument("-g", "--gff", help="input gff file")
 parser.add_argument("-s", "--genome", help="input genome fasta file")
 parser.add_argument("-l", "--length", help="maximum promoter length")
-parser.add_argument("-p", "--prevent-overlapping",action='store_true', help="prevents promoters of different genes to overlap one another. When two promoters would overlap each other the distance between them is equally distributed")
+parser.add_argument("-p", "--prevent_overlapping",action='store_true', help="prevents promoters of different genes to overlap one another. When two promoters would overlap each other the distance between them is equally distributed")
 parser.add_argument("-o", "--output", help="output gff file")
 
 args = parser.parse_args()
@@ -55,7 +55,7 @@ for record in SeqIO.parse(open(genome), "fasta"):
                             line['start'] = promline['start']
                 elif prevline['sense'] == '-':
                     try:
-                        if args.prevent-overlapping == True:
+                        if args.prevent_overlapping == True:
                     # This part is to make sure to not have overlapping promoters.
                     # When two promoters meet each other the distance between them is equally distributed.
                     # The finaldata of the previous line is changed accordingly
@@ -76,7 +76,7 @@ for record in SeqIO.parse(open(genome), "fasta"):
                                     promprevline['end'] = prevline['end']
                                     promline['start'] = promline['end'] - 1
                                     line['start'] = promline['start']
-                    except NameError:
+                    except AttributeError:
                         pass
         elif line['sense'] == '-' :
             if (line['end'] + length) < len(record.seq):
