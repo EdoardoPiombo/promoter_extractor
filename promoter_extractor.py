1346a33b9e0f16dfd0fd7c08b0457f25a560ee19
EdoardoPiombo <edoardo.piombo@gmail.com>
2023-02-14 14:55:09 +0100

    Update promoter_extractor.py

--- promoter_extractor.py
+++ promoter_extractor.py
@@ -21,6 +21,7 @@ length = int(args.length)
 data = pd.read_csv(gff, sep = '\t', names = ['scaffold', 'predictor', 'feature', 'start', 'end', 'thing', 'sense', 'RF', 'description'], comment = '#')
 data = data.where(data['feature'] == 'gene').dropna()
 scaflist = data['scaffold'].drop_duplicates().tolist()
+data['to_remove'] = 'No'
 
 # 2 dataframes are created, one for promoters and one for genes, and then they are both used to create the final one
 finaldata = pd.DataFrame(columns = data.columns)
@@ -42,6 +43,10 @@ for record in SeqIO.parse(open(genome), "fasta"):
             elif line['start'] <= length :
                 promline['end'] = promline['start'] -1
                 promline['start'] = 1
+                # genes that start at the first base have no promoter as there are no bases before the gene
+                if line['start'] == 1:
+                    line['to_remove'] = 'yes'
+                    promline['to_remove'] = 'yes'
                 line['start'] = promline['start']
             if int(id) > 0:
                 prevline = scafdata.loc[id-1]
@@ -86,6 +91,10 @@ for record in SeqIO.parse(open(genome), "fasta"):
             else:
                 promline['start'] = promline['end'] +1
                 promline['end'] = len(record.seq)
+                # genes on the minus filament which start at the end of the scaffold have no promoter as there are no bases after the gene
+                if line['end'] == len(record.seq) :
+                    line['to_remove'] = 'yes'
+                    promline['to_remove'] = 'yes'
                 line['end'] = promline['end']                
             try:
                 nextline = scafdata.loc[id+1]
@@ -110,13 +119,16 @@ for record in SeqIO.parse(open(genome), "fasta"):
         if promline['start'] <= 0:
             promline['start'] = 1
         if promline['start'] >= promline['end']:
-            promline['end'] = promline['start'] + 1
+            promline['end'] = promline['start']
         scafdata.loc[id] = line
         scafpromdata.loc[id] = promline
         finalid +=2
         finaldata.loc[finalid-1] = promline
         finaldata.loc[finalid] = line
-        
+
+droppable = finaldata.where(finaldata['to_remove'] == 'yes').dropna().index.to_list()
+finaldata.drop(['to_remove'], axis = 1, inplace = True)
+finaldata.drop(droppable, axis = 0, inplace = True)
 finaldata = finaldata.reset_index(drop = True)
         
 finaldata.to_csv(path_or_buf=output, sep = '\t', index =False, header=False)
